f8610c0e2d24d1c45d56cf4e3ee39d72d2c12b77
Sofia Vanessa <105688383+sofiat25@users.noreply.github.com>
2023-10-25 06:01:49 +0000

    card generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let icons = [
+  { key: "♦", color: "red" },
+  { key: "♥", color: "red" },
+  { key: "♠", color: "black" },
+  { key: "♣", color: "black" }
+];
+
+const iconTop = document.querySelector(".top-icon");
+const iconBottom = document.querySelector(".bottom-icon");
+const cardValue = document.querySelector(".number");
+const btnGenerator = document.querySelector(".generate");
+
+btnGenerator.addEventListener("click", generateCard);
+
+function generateCard() {
+  let randomNum = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomIcon = icons[Math.floor(Math.random() * icons.length)];
+
+  iconTop.innerHTML = randomIcon.key;
+  iconBottom.innerHTML = randomIcon.key;
+  iconTop.style.color = randomIcon.color;
+  iconBottom.style.color = randomIcon.color;
+  cardValue.innerHTML = randomNum;
+}
